79a376350bf148125b5ee54b80fd495ba8aef239
Bahri.Busch <turkbahri19@hotmail.com>
2024-01-03 02:13:07 +0300

    auth added

--- case/src/app/app.component.ts
+++ case/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  
+  }
   title = 'case';
+  object ={
+    id:1,
+    name:"test",
+    surname:"ahraz",
+    age:31
+  }
+  
 }
--- case/src/components/login/login.component.ts
+++ case/src/components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/User';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  user:User = { userName: '', password: '' };
+  constructor(private authenticationService:AuthenticationService){
+  }
+  login() {
+    const isLoggedIn = this.authenticationService.login(this.user);
+    if (isLoggedIn) {
+      console.log('Giriş başarılı!');
+    } else {
+      console.log('Giriş başarısız!');
+    }
+  }
 }
